5a49bea9014efc01d4ce808d8769e43a67fe6307
HOANG PHI HUNG <hung.t174800@vanlanguni.vn>
2019-11-19 23:22:43 +0700

    update ver0.3

---
+++ SixstarPPC/SixstarPPC/Areas/AdminPPC/AdminPPCAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SixstarPPC.Areas.AdminPPC
+{
+    public class AdminPPCAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "AdminPPC";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "AdminPPC_default",
+                "AdminPPC/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
